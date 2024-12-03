0a345fa27896f688af962da6e5661628abff1d87
Kalle M. Aagaard <km@conscia.com>
2024-12-03 09:16:26 +0100

    Version updated from 1.0.1.4 to 1.0.2

--- extensions.py
+++ extensions.py
@@ -1,4 +1,5 @@
 import subprocess
+from datetime import date
 
 from jinja2 import Environment
 from jinja2.ext import Extension
@@ -17,3 +18,9 @@ class GitExtension(Extension):
         super().__init__(environment)
         environment.filters["git_user_name"] = git_user_name  # type: ignore
         environment.filters["git_user_email"] = git_user_email  # type: ignore
+
+
+class CurrentYearExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+        environment.globals["current_year"] = date.today().year  # type: ignore
