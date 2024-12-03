d33947551e342d0d815c225de46898f04518ce5f
Kalle M. Aagaard <km@conscia.com>
2024-12-03 12:59:04 +0100

    flaf

--- extensions.py
+++ extensions.py
@@ -13,6 +13,15 @@ def git_user_email(default: str) -> str:
     return subprocess.getoutput("git config user.email").strip() or default
 
 
+def github_user(default: str) -> str:
+    return (
+        subprocess.getoutput(
+            "gh api -H 'Accept: application/vnd.github+json' /user -q .login"
+        ).strip()
+        or default
+    )
+
+
 class GitExtension(Extension):
     def __init__(self, environment: Environment):
         super().__init__(environment)
@@ -24,3 +33,9 @@ class CurrentYearExtension(Extension):
     def __init__(self, environment: Environment):
         super().__init__(environment)
         environment.globals["current_year"] = date.today().year  # type: ignore
+
+
+class GithubExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+        environment.filters["github_user_name"] = github_user  # type: ignore
